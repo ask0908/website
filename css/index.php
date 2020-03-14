5552fc98814da2b021c874d9c9ce42e36e141f27
ask0908 <59863631+ask0908@users.noreply.github.com>
2020-03-15 03:10:59 +0900

    initial commit in desktop

---
+++ css/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=chrome">
+    <meta name="viewport" content="width=device-width", initial-scale="1">
+    <title>제목</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+  </head>
+  <body>
+    <style type="text/css">
+      .jumbotron {
+        background-image: url('img/background.jpg');
+        background-size: cover;
+        text-shadow: black 0.2em 0.2em 0.2em;
+        color: white;
+      }
+    </style>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapsed"
+          data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">코딩 부스터</a>
+        </div>
+        <div class="collapse navbar-colapse" id="bs-example-navbar-collapse-1">
+          <ul class="navbar-nav">
+            <li class="active"><a href="#">소개<span class="sr-only"></span></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="jumbotron">
+        <h1 class="text-center">코딩 부스터를 소개합니다</h1>
+        <p class="text-center">코딩 부스터는 IT 교육 사이트이다. 다양한 IT 관련 무료, 유료 강의가 존재함</p>
+        <p class="text-center"><a class="btn btn-primary btn-lg" href="#" role="button">강의 들으러 가기</a></p>
+      </div>
+    </div>
+    <!-- <div>
+      <button type="button">버튼</button>
+      <button type="button" class="btn">BS 버튼 1</button>
+      <button type="button" class="btn btn-primary">BS primary 버튼</button>
+      <button type="button" class="btn btn-default">BS default 버튼</button>
+      <button type="button" class="btn btn-success">BS success 버튼</button>
+      <button type="button" class="btn btn-warning">BS warning 버튼</button>
+      <button type="button" class="btn btn-info">BS info 버튼</button>
+      <button type="button" class="btn btn-danger">BS danger 버튼</button>
+    </div>
+    <div>
+      <button type="button" class="btn btn-primary btn-lg">BS primary 버튼</button>
+      <button type="button" class="btn btn-default btn-lg">BS default 버튼</button>
+      <button type="button" class="btn btn-success btn-lg">BS success 버튼</button>
+      <button type="button" class="btn btn-warning btn-lg">BS warning 버튼</button>
+      <button type="button" class="btn btn-info btn-lg">BS info 버튼</button>
+      <button type="button" class="btn btn-danger btn-lg">BS danger 버튼</button>
+    </div> -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>테스트</title>
+  </head>
+  <body>
+    <h1>테스트</h1>
+  </body>
+</html>
